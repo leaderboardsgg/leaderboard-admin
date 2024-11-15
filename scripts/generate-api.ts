e587c0c3fe6c186b1f2c01997745b63b2d984468
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-11-15 18:04:15 -0500

    Add swagger typescript api.

---
+++ scripts/generate-api.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'node:path'
+import { config } from 'dotenv'
+import { generateApi } from 'swagger-typescript-api'
+
+config({
+	path: [resolve(process.cwd(), '.env'), resolve(process.cwd(), '.env.local')],
+})
+
+const backendUrl = process.env.VITE_BACKEND_URL
+
+if (!backendUrl) {
+	throw new Error('Backend URL not set!')
+}
+
+generateApi({
+	url: `${backendUrl}/swagger/v1/swagger.json`,
+	output: resolve(process.cwd(), 'src', 'api'),
+})
