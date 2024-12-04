e06942f3113b83153eca0f93ec5e35fbee1e71d0
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-12-04 17:55:49 -0500

    Show login page if not signed in as admin.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import Login from './components/Login.vue';
 import Navbar from './components/Navbar.vue'
-import Main from './components/Main.vue'
+import { useSessionToken } from './composables/useSessionToken';
+import { useUserDetails } from './composables/useUserDetails';
+
+const token = useSessionToken()
+const user = useUserDetails()
+const loggedIn = computed(() => token.value && user.value?.role === 'Administrator')
 </script>
 
 <template>
 	<div class="root">
-		<Navbar />
-		<RouterView />
+		<div v-if="loggedIn">
+			<Navbar />
+			<RouterView />
+		</div>
+		<Login v-else />
 	</div>
 </template>
 
