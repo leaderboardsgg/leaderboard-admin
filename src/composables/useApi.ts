df0d326176c80b82618b5c3076ab01b649d8612f
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-12-06 14:35:59 -0500

    Use more concise typing.

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -1,14 +1,17 @@
 import type { HttpResponse } from '../lib/api/http-client'
 
-export async function useApi<D, E, T extends Promise<HttpResponse<D, E>>>(
-	act: () => T,
-	onSuccess: (data: Omit<Awaited<T>, 'error'>) => unknown,
-	onError: (error: Omit<Awaited<T>, 'data'>) => unknown,
+type SuccessResponse<T> = Omit<HttpResponse<T, unknown>, 'error'>
+type ErrorResponse<T> = Omit<HttpResponse<unknown, T>, 'data'>
+
+export async function useApi<D, E>(
+	act: () => Promise<HttpResponse<D, E>>,
+	onSuccess: (data: SuccessResponse<D>) => unknown,
+	onError: (error: ErrorResponse<E>) => unknown,
 ) {
 	try {
 		const response = await act()
 		onSuccess(response)
 	} catch (error) {
-		onError(error as Omit<Awaited<T>, 'data'>)
+		onError(error as ErrorResponse<E>)
 	}
 }
