db77a7f8d71d9c3c198bf88005a99e4757aa2c24
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-12-07 17:00:30 -0500

    Move v-for onto the proper tag.

--- src/components/Leaderboards.vue
+++ src/components/Leaderboards.vue
@@ -63,9 +63,11 @@ const boards = computed(() =>
 			<button @click="execute()" class="reload-button">Retry</button>
 		</div>
 
-		<ul v-else v-for="board in boards" :key="board.id" class="main-content">
+		<ul v-else class="main-content">
 			<!-- TODO: Switch list item to RouterLink -->
-			<li>{{ board.name }}</li>
+			<li v-for="board in boards" :key="board.id">
+				{{ board.name }}
+			</li>
 			<!-- <RouterLink :to="{
 				name: 'leaderboard',
 				params: { slug: board.slug },
