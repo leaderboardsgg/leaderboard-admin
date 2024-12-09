0a9cbc6f5955d535449f211497ba9b0c06096d46
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-12-09 18:28:26 -0500

    Use global state for user details.

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,38 @@ import Login from './components/Login.vue'
 import Navbar from './components/Navbar.vue'
 import { useUserDetails } from './composables/useUserDetails'
 
-const user = useUserDetails()
-const loggedIn = computed(() => user.value?.role === 'Administrator')
+const { state, isLoading, isReady } = useUserDetails()
+const loggedIn = computed(
+	() => state.value?.role === 'Administrator'
+)
 </script>
 
 <template>
 	<div class="root">
-		<div v-if="loggedIn">
-			<Navbar />
-			<RouterView />
+		<Teleport to="body" v-if="isLoading">
+			<div class="loader"><p>Loading...</p></div>
+		</Teleport>
+		<div v-show="isReady">
+			<template v-if="loggedIn">
+				<Navbar />
+				<RouterView />
+			</template>
+			<Login v-else />
 		</div>
-		<Login v-else />
 	</div>
 </template>
 
 <style scoped>
 .root {
 	width: 100%;
+}
+
+.loader {
+	position: fixed;
+	z-index: 999;
+	top: 45%;
+	left: 50%;
+	padding: 1rem;
+	background-color: black;
 }
 </style>
